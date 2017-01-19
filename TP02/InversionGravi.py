8024574020c5c1891ffaae2ea41bd400bff37eb1
clberube <charleslberube@gmail.com>
2017-01-18 21:03:24 -0500

    TP02 inversion

--- TP02/InversionGravi.py
+++ TP02/InversionGravi.py
@@ -8,9 +8,14 @@ Created on Tue Jan 17 20:03:36 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
-axeX = np.arange(0,300)*100
+def grav_cylindre(x, rho, r, p_x, p_z, G=6.674e-8):
+    return ((2 * np.pi * G * rho * r**2) / p_z ) / (1 + (((p_x - x)/p_z)**2))
 
-G = 6.674e-8
+def grav_sphere(x, rho, r, p_x, p_z, G=6.674e-8):
+    return (4./3)*np.pi*G*rho*p_z*r**3 / ( ((p_x - x)**2 + p_z**2)**(3./2) )
+
+# Axe des X et paramètres intiaux pour calculer la réponse des corps simples
+axeX = np.arange(0,500)*100
 
 rho_1 = 3.78
 R_1 = 12.8*100
@@ -22,63 +27,51 @@ R_2 = 16.3*100
 PosX_2 = 200*100
 PosZ_2 = 23*100
 
-rho_3 = 2.21
-R_3 = 15.0
-PosX_3 = 150
-PosZ_3 = 20
+rho_3 = -2.67
+R_3 = 10.0*100
+PosX_3 = 350*100
+PosZ_3 = 21.5*100
 
-rho_4 = -2.0
-R_4 = 0.05
-PosX_4 = 200
-PosZ_4 = 5
+# Générer le data synthétique à partir des paramètres initiaux
+g_1 = grav_sphere(axeX, rho_1, R_1, PosX_1, PosZ_1)
+g_2 = grav_sphere(axeX, rho_2, R_2, PosX_2, PosZ_2)
+g_3 = grav_cylindre(axeX, rho_3, R_3, PosX_3, PosZ_3)
+g_data = g_1 + g_2 + g_3
+# Ajouter un bruit Gaussien de 2%
+g_data += np.random.normal(0,0.02*max(abs(g_data)),size=len(g_data)) 
 
-def grav_cylindre(x, G, rho, r, p_x, p_z):
-    return (2 * np.pi * G * rho * r**2) / (p_z * (1 + ((p_x - x)/p_z)**2))
+# Générer des listes vides pour entreposer les paramètres choisis
+param1 = []
+param2 = []
+param3 = []
+RMS_list = []
 
-def grav_sphere(x, G, rho, r, p_x, p_z):
-    return (4./3)*np.pi*G*rho*p_z*r**3 / ( ((p_x - x)**2 + p_z**2)**(3./2) )
+# Tester N hypothèses et calculer la fonction objectif 
+for i in range(100000):
+    # Générer des paramètres de façon aléatoire
+    rho_mod_1 = np.random.uniform(-5,5)
+    R_mod_2 = np.random.uniform(10,20)*100
+    PosZ_mod_3 = np.random.uniform(15,25)*100
+    # Calculer le modèle proposé
+    g_model = grav_sphere(axeX, rho_mod_1, R_1, PosX_1, PosZ_1) + grav_sphere(axeX, rho_2, R_mod_2, PosX_2, PosZ_2) + grav_cylindre(axeX, rho_3, R_3, PosX_3, PosZ_mod_3)
+    # La fonction objectif (Root Mean Square Error)
+    RMS = np.sqrt(((g_model - g_data) ** 2).mean()) 
+    # Entreposer les paramètres si c'est la première itération ou si la 
+    # nouvelle hypothèse est plus probable que la précédente
+    if (i == 0) or (RMS < RMS_list[-1]):
+        RMS_list.append(RMS)
+        param1.append(rho_mod_1)
+        param2.append(R_mod_2)
+        param3.append(PosZ_mod_3)
+   
+# Tracer le graphique du meilleur modèle
+for i in range(len(param1)):
+    best_g_model = grav_sphere(axeX, param1[i], R_1, PosX_1, PosZ_1) + grav_sphere(axeX, rho_2, param2[i], PosX_2, PosZ_2) + grav_cylindre(axeX, rho_3, R_3, PosX_3, param3[i])
+    plt.figure()
+    plt.plot(axeX/100, 1000*g_data, 'bo')
+    plt.plot(axeX/100, 1000*best_g_model, 'r-')
+    plt.xlabel("Position")
+    plt.ylabel("mGal")
     
-g_1 = grav_sphere(axeX, G, rho_1, R_1, PosX_1, PosZ_1)
-
-g_2 = grav_sphere(axeX, G, rho_2, R_2, PosX_2, PosZ_2)
-
-g_3 = grav_sphere(axeX, G, rho_3, R_3, PosX_3, PosZ_3)
-
-g_4 = grav_cylindre(axeX, G, rho_4, R_4, PosX_4, PosZ_4)
-
-g_data = g_1 + g_2 
-
-g_data += np.random.normal(0,0.01*max(g),size=len(g))
-
-def rmse(predictions, targets):
-    return np.sqrt(((predictions - targets) ** 2).mean())
-
-g_model = []
-#for i in range(1000):
-    
-while True:
-    
-    rho_mod_1 = np.random.uniform(0,5)
-    R_mod_1 = np.random.uniform(5,15)
-    
-    rho_mod_2 = np.random.uniform(-5,5)
-    R_mod_2 = np.random.uniform(5,15)
-    
-    print rho_mod_1
-#    print rho_mod_2
-    
-    g_model.append(grav_sphere(axeX, G, rho_mod_1, R_1, PosX_1, PosZ_1) + grav_sphere(axeX, G, rho_2, R_2, PosX_2, PosZ_2))
-
-    RMS = np.sqrt(((g_model - g_data) ** 2).mean())
-    print RMS
-    if RMS < 0.0003:
-        break
-    
-    #    print np.sum(g_model-g_data)
-
-#plt.plot(g_model[-1])
-    
-plt.plot(axeX/100, 1000*g_data, 'bo')
-plt.plot(axeX/100, 1000*g_model[-1], 'r-')
-plt.xlabel("Position")
-plt.ylabel("mGal")
+plt.figure()
+plt.plot(RMS_list)
